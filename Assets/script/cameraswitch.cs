0587d39333e6f5935454b95c260aedd2b96d998b
chaudharybharat <bharatchaudhary20893@gmail.com>
2019-07-06 11:06:53 +0530

    first commit

---
+++ Assets/script/cameraswitch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraswitch : MonoBehaviour {
+
+    [SerializeField]
+    Camera upcamera;
+    [SerializeField]
+    Camera secondcamera;
+    // Use this for initialization
+    void Start () {
+        upcamera.GetComponent<Camera>().enabled = true;
+        secondcamera.GetComponent<Camera>().enabled = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("t"))
+        {
+            Debug.Log("textnfnlnfgnlffg");
+            upcamera.GetComponent<Camera>().enabled = false;
+            secondcamera.GetComponent<Camera>().enabled = true;
+        }
+        if (Input.GetKeyDown("w"))
+        {
+            Debug.Log("textnfnlnfgnlffg");
+            upcamera.GetComponent<Camera>().enabled = true;
+            secondcamera.GetComponent<Camera>().enabled = false;
+        }
+    }
+}
